0a0c4df9c20f2f4c25cf6fef243652d1ad8854b8
Orkun Alp Ünlü <orkunalp98@gmail.com>
2022-10-23 21:46:57 +0300

    Player Attack added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,14 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
+    public SwordAttack swordAttack;
     Vector2 movementInput;
     Rigidbody2D rigidbody;
     Animator animator;
     SpriteRenderer spriteRenderer;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,52 +27,58 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        if (!canMove) return;
+
         // If movement input is not 0 try to move
         if (movementInput != Vector2.zero) {
             // Check for potential collisions
             bool success = TryMove(movementInput);
-
-            if (!success) {
-                success = TryMove(new Vector2(movementInput.x, 0));
-            }
-            if (!success) {
-                success = TryMove(new Vector2(0, movementInput.y));
-            }
+            if (!success) success = TryMove(new Vector2(movementInput.x, 0));
+            if (!success) success = TryMove(new Vector2(0, movementInput.y));
 
             animator.SetBool("isMoving", success);
-        } else {
-            animator.SetBool("isMoving", false);
-        }
+        } else animator.SetBool("isMoving", false);
 
         // Set the direction of sprite to movement direction
-        if (movementInput.x < 0) {
-            spriteRenderer.flipX = true;
-        } else if (movementInput.x > 0) {
-            spriteRenderer.flipX = false;
-        }
+        if (movementInput.x < 0) spriteRenderer.flipX = true;
+        else if (movementInput.x > 0) spriteRenderer.flipX = false;
     }
 
     private bool TryMove(Vector2 direction) {
-        if (direction != Vector2.zero) {
+        if (direction == Vector2.zero) return false; // Can't move if there's no direction to move in
             int count = rigidbody.Cast(
                 direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
                 movementFilter, // The settings that determine where a collision can occur on such as layers can collide with
                 castCollisions, // List of collisions to store the found collisions into after the Cast is finished
                 moveSpeed * Time.fixedDeltaTime + collisionOffset // The amount to cast equal to the movement plus an offset
             );
-            if (count == 0) {
+            if (count != 0) return false;
+            else {
                 rigidbody.MovePosition(rigidbody.position + direction * moveSpeed * Time.fixedDeltaTime);
                 return true;
-            } else {
-                return false;
             }
-        } else {
-            // Can't move if there's no direction to move in
-            return false;
-        }
     }
 
     void OnMove(InputValue movementValue) {
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    void OnFire() {
+        animator.SetTrigger("swordAttack");
+    }
+
+    public void SwordAttack() {
+        LockMovement();
+        
+        if (spriteRenderer.flipX) swordAttack.AttackLeft();
+        else swordAttack.AttackRight();
+    }
+
+    public void LockMovement() {
+        canMove = false;
+    }
+
+    public void UnlockMovement() {
+        canMove = true;
     }
 }
---
+++ Assets/Scripts/SwordAttack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    Vector2 rightAttackOffset;
+    Collider2D swordCollider;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        swordCollider = GetComponent<Collider2D>();
+        rightAttackOffset = transform.position;
+    }
+
+    public void AttackRight() {
+        swordCollider.enabled = true;
+        transform.position = rightAttackOffset;
+    }
+
+    public void AttackLeft() {
+        swordCollider.enabled = true;
+        transform.position = new Vector2(rightAttackOffset.x * -1, rightAttackOffset.y);
+    }
+
+    public void StopAttack() {
+        swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy") {
+            // Deal damage to the enemy
+        }
+    }
+}
